a29ae8c31957723b86efc77d7663e4929eccdf5d
Henry Muhiar <hamzeh.k.muhiar@gmail.com>
2025-06-14 14:02:15 -0400

    making standalone API server
    attempting to detach the function of exposing data to API server from data_fetcher.py to a standalone api_server.py

---
+++ api_server.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from credential_fetcher import get_secret
+
+def serve_data():
+    app = Flask(__name__)
+    
+    qubic_username, qubic_password, mongo_username, mongo_password = get_secret()
+    print(mongo_password, mongo_username)
+
+    # MongoDB connection
+    uri = f"mongodb+srv://{mongo_username}:{mongo_password}@cluster0.4s0bnni.mongodb.net/?retryWrites=true&w=majority"
+
+    client = MongoClient(uri)
+    db = client["qubic_dashboard"]
+    collection = db["metrics"]
+
+    @app.route('/data', methods=['GET'])
+    def get_data():
+        data = list(collection.find({}, {'_id': 0}).sort("time_stamp", -1))  # Exclude MongoDB's default _id field
+        return jsonify(data)
+
+        # Start Flask server
+    app.run(host='0.0.0.0', port=5001)
+    
+if __name__ == '__main__':
+    serve_data()
